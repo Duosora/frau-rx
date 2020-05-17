573dd9348f233cd0acaff18dddc1f5e152901221
Duosora <ergi.duoson@gmail.com>
2020-05-17 16:18:24 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,7 @@ import React from 'react';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import './react-tabs.css';
 import './App.css';
-//import Gasp from './Gasp';
 import config from './utils/config';
-//import { gapi } from 'gapi-script';
-
-//import { loadGasp, loadStrings } from './utils/sheet';
-
-
-
 
 class App extends React.Component {
   constructor() {
@@ -20,42 +13,12 @@ class App extends React.Component {
     }
   }
   componentDidMount() {
-    //gapi.load('client', this.initClient);
+
   };
   
   initClient = () => {
 	  console.log('inits');
-    /*gapi.client.init({
-      apiKey: config.apiKey,
-      discoveryDocs: config.discoveryDocs,
-      clientId: config.clientId,
-      scope: "https://www.googleapis.com/auth/spreadsheets.readonly"
-    }).then(() => {
-      //loadGasp(this.setGasp);
-      //loadStrings(this.setStrings);
-    }, response => console.log(response)); */
   }
-
-/*
-  setGasp = (data, error) => {
-    console.log(error);
-    if(error) {
-      console.log(error);
-    }
-    else {
-      this.setState({gasp: data});
-    }
-  };
-  setStrings = (data, error) => {
-    if(error) {
-      console.log(error);
-    }
-    else {
-      this.setState({strings: data});
-    }
-  };
-  */
-
   render() {
     return <div className="App">
         <Tabs>
@@ -63,8 +26,7 @@ class App extends React.Component {
             <Tab>GASP</Tab>
           </TabList>
           <TabPanel>
-			<div>hello</div>
-            //<Gasp sheet={this.state.gasp} strings={this.state.strings} />
+
           </TabPanel>
         </Tabs>
       </div>;
